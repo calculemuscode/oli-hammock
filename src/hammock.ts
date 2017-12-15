a571e4fc507e0b65f85fb3ae6678715ae6972dd8
Rob Simmons <rob@calculem.us>
2017-12-15 16:23:01 -0500

    Refactor the execution logic into a Runner object

--- src/hammock.ts
+++ src/hammock.ts
@@ -2,6 +2,7 @@ import { SuperActivity, SuperActivityClient } from "./superactivity";
 import { Activity, QuestionData, PartData, FeedbackData } from "./activity";
 import { readAssets } from "./assets";
 import { QuestionInt, QuestionsInt, PartInt, validateQuestions } from "./int";
+import { Runner } from "./runner";
 
 /**
  * Attach boilerplate elements to the page
@@ -19,238 +20,32 @@ function initializeHTML(assets: Map<string, Element>): void {
     if (layout) $("#oli-embed").append(layout);
 }
 
-/**
- * Implements the grading match-up logic
- */
-function grade(question: QuestionInt, responses: (string | null)[]): (FeedbackData | null)[] {
-    return responses.map((response, i) => {
-        if (!response) return null;
-
-        const match = question.parts[i].match.get(response);
-        if (!match) {
-            const nomatch = question.parts[i].nomatch;
-            if (!nomatch) throw new Error(`grade: question ${i} has no match for ${response}`);
-
-            return {
-                correct: nomatch.score === question.parts[i].score,
-                score: nomatch.score,
-                message: nomatch.message
-            };
-        } else {
-            return {
-                correct: match.score === question.parts[i].score,
-                score: match.score,
-                message: match.message
-            };
-        }
-    });
-}
-
-function constructQuestionData<UserDefinedData>(
-    question: QuestionInt,
-    user: UserDefinedData,
-    feedback: (FeedbackData | null)[]
-): QuestionData<UserDefinedData> {
-    const questionData: QuestionData<UserDefinedData> = {
-        state: user,
-        parts: question.parts.map((part: PartInt, i): PartData => {
-            const partData: PartData = {};
-            const thisFeedback = feedback[i];
-            if (part.prompt) partData.prompt = part.prompt;
-            if (part.hints) partData.hints = part.hints.map(x => x); // Deep copy
-            if (thisFeedback) partData.feedback = thisFeedback;
-            return partData;
-        })
-    };
-
-    if (question.prompt) questionData.prompt = question.prompt;
-    if (question.hints) questionData.hints = question.hints.map(x => x);
-
-    return questionData;
-}
-
-function readSavedData<UserDefinedData>(
-    activity: Activity<UserDefinedData>,
-    superActivity: SuperActivity,
-    activityData: Element,
-    questionArray: QuestionsInt
-): Promise<{ userdata: UserDefinedData[]; feedback: (FeedbackData | null)[][]; question: QuestionsInt }> {
-    const defaults = () =>
-        Promise.resolve({
-            userdata: questionArray.map(() => activity.init()),
-            feedback: questionArray.map(question => question.parts.map(() => null)),
-            question: questionArray
-        });
-
-    if (typeof superActivity.currentAttempt === "string") {
-        superActivity.currentAttempt = parseInt(superActivity.currentAttempt);
-    }
-
-    if (superActivity.currentAttempt == 1) {
-        return defaults();
-    }
-
-    const savedData = new Map<string, any>();
-    const promises = new Map<string, Promise<void>>();
-
-    $(superActivity.sessionData)
-        .find("storage file_directory file_record")
-        .each((index, record) => {
-            const attempt = parseInt(
-                $(record)
-                    .find("record_context")
-                    .attr("attempt") || "-1"
-            );
-            const filename = $(record).attr("file_name") || "undefined";
-            if (attempt + 1 === superActivity.currentAttempt) {
-                promises.set(
-                    filename,
-                    new Promise(resolve => {
-                        superActivity.loadFileRecord(
-                            filename,
-                            parseInt(superActivity.currentAttempt.toString()) - 1, // xxx redundant parseInt
-                            (result: any) => {
-                                savedData.set(filename, result);
-                                resolve();
-                            }
-                        );
-                    })
-                );
-            }
-        });
-
-    if (promises.has("reset")) {
-        return defaults();
-    } else if (promises.has("userdata") && promises.has("feedback")) {
-        return Promise.all(promises.values()).then(() => ({
-            userdata: savedData.get("userdata"),
-            feedback: savedData.get("feedback"),
-            question: questionArray
-        }));
-    } else {
-        console.error("Not first attempt, but there is no saved data");
-        return defaults();
-    }
-}
-
-export function hammock<UserDefinedData>(activity: Activity<UserDefinedData>): SuperActivityClient {
+export function hammock<UserData>(activity: Activity<UserData>): SuperActivityClient {
     return {
-        init: function(superActivity: SuperActivity, activityData: Element) {
-            if (superActivity.currentAttempt === "none") {
-                setTimeout(() => console.log(`Delayed: ${superActivity.currentAttempt}`), 5000);
-                throw new Error("Error in superactivity: currentAttempt non-numeric");
-            }
+        init: (superActivity: SuperActivity, activityData: Element): void => {
+            readAssets(superActivity.webContentFolder, activityData).then(assets => {
+                initializeHTML(assets);
+                const questions = validateQuestions(assets.get("questions"));
+                const runner = new Runner<UserData>(activity, superActivity, activityData, questions);
+                runner.readSavedData().then(() => {
 
-            readAssets(superActivity.webContentFolder, activityData)
-                .then(assets => {
-                    initializeHTML(assets);
-
-                    // XXX TODO: deal with possibility of a nonexistant questions asset (default or error)?
-                    return readSavedData(
-                        activity,
-                        superActivity,
-                        activityData,
-                        validateQuestions(assets.get("questions"))
-                    );
-                })
-                .then(data => {
-                    let currentIndex = 0;
-                    const userDataArray = data.userdata;
-                    const feedbackArray = data.feedback;
-                    const questionArray = data.question;
-
-                    const render = () =>
-                        activity.render(
-                            constructQuestionData(
-                                questionArray[currentIndex],
-                                userDataArray[currentIndex],
-                                feedbackArray[currentIndex]
-                            )
-                        );
-
-                    const btnSubmit: JQuery<HTMLElement> = $("<button/>", {
+                    $("#oli-embed").append($("<button/>", {
                         class: "btn btn-primary btn-sm",
                         text: "SUBMIT",
-                        click: (): void => {
-                            userDataArray[currentIndex] = activity.read();
-                            feedbackArray[currentIndex] = grade(
-                                questionArray[currentIndex],
-                                activity.parse(userDataArray[currentIndex])
-                            );
+                        click: () => runner.submit()
+                    }));
 
-                            superActivity.writeFileRecord(
-                                "userdata",
-                                "application/json",
-                                superActivity.currentAttempt,
-                                JSON.stringify(userDataArray),
-                                () => {
-                                    superActivity.writeFileRecord(
-                                        "feedback",
-                                        "application/json",
-                                        superActivity.currentAttempt,
-                                        JSON.stringify(feedbackArray),
-                                        () => {
-                                            superActivity.scoreAttempt(
-                                                "percent",
-                                                75, // XX TODO FIX
-                                                () => {
-                                                    superActivity.endAttempt(() => {
-                                                        superActivity.startAttempt((response: Element) => {
-                                                            const newAttempt = parseInt(
-                                                                $(response)
-                                                                    .find("attempt_history")
-                                                                    .attr("current_attempt") || "-1"
-                                                            );
-                                                            superActivity.currentAttempt = newAttempt;
-                                                        });
-                                                    });
-                                                }
-                                            );
-                                        }
-                                    );
-                                }
-                            );
-
-                            render();
-                        }
-                    });
-
-                    const btnReset: JQuery<HTMLElement> = $("<button/>", {
+                    $("#oli-embed").append($("<button/>", {
                         class: "btn btn-primary btn-sm",
                         text: "RESET",
-                        click: () => {
-                            userDataArray[currentIndex] = activity.init();
-                            feedbackArray[currentIndex] = questionArray[currentIndex].parts.map(() => null);
+                        click: () => runner.reset()
+                    }));
 
-                            superActivity.writeFileRecord(
-                                "reset",
-                                "application/json",
-                                superActivity.currentAttempt,
-                                JSON.stringify(userDataArray),
-                                () => {
-                                    superActivity.endAttempt(() => {
-                                        superActivity.startAttempt((response: Element) => {
-                                            const newAttempt = parseInt(
-                                                $(response)
-                                                    .find("attempt_history")
-                                                    .attr("current_attempt") || "-1"
-                                            );
-                                            superActivity.currentAttempt = newAttempt;
-                                        });
-                                    });
-                                }
-                            );
+                    runner.render();
 
-                            render();
-                        }
-                    });
-
-                    $("#oli-embed")
-                        .append(btnSubmit)
-                        .append(btnReset);
-
-                    render();
                 });
+            });
         }
     };
 }
+
---
+++ src/runner.ts
@@ -0,0 +1,174 @@
+import { SuperActivity } from "./superactivity";
+import { Activity, QuestionData, PartData, FeedbackData } from "./activity";
+import { QuestionInt, PartInt } from "./int";
+
+export class Runner<UserDefinedData> {
+    private currentIndex: number;
+    private currentAttempt: number;
+    private activity: Activity<UserDefinedData>;
+    private superActivity: SuperActivity;
+    private userDataArray: UserDefinedData[];
+    private feedbackArray: (FeedbackData | null)[][];
+    private questionArray: QuestionInt[];
+
+    constructor(activity: Activity<UserDefinedData>, superActivity: SuperActivity, activityData: Element, questionArray: QuestionInt[]) {
+        this.activity = activity;
+        this.superActivity = superActivity;
+        if (superActivity.currentAttempt === "none") {
+            setTimeout(() => console.log(`Delayed: ${superActivity.currentAttempt}`), 5000);
+            throw new Error("Error in superactivity: currentAttempt non-numeric");
+        }
+        this.currentAttempt = parseInt(superActivity.currentAttempt.toString());
+
+        this.currentIndex = 0;
+        this.questionArray = questionArray;
+        this.userDataArray = questionArray.map(() => activity.init());
+        this.feedbackArray = questionArray.map(question => question.parts.map(() => null));
+    }
+
+    private constructQuestionData(): QuestionData<UserDefinedData> {
+        const questionData: QuestionData<UserDefinedData> = {
+            state: this.userDataArray[this.currentIndex],
+            parts: this.questionArray[this.currentIndex].parts.map((part: PartInt, i): PartData => {
+                const partData: PartData = {};
+                const thisFeedback = this.feedbackArray[this.currentIndex][i];
+                if (part.prompt) partData.prompt = part.prompt;
+                if (part.hints) partData.hints = part.hints.map(x => x); // Deep copy
+                if (thisFeedback) partData.feedback = thisFeedback;
+                return partData;
+            })
+        };
+
+        if (this.questionArray[this.currentIndex].prompt) {
+            questionData.prompt = this.questionArray[this.currentIndex].prompt;
+        }
+        if (this.questionArray[this.currentIndex].hints) {
+            questionData.hints = this.questionArray[this.currentIndex].hints;
+        }
+
+        return questionData;
+    }
+
+    render(): void {
+        this.activity.render(this.constructQuestionData());
+    }
+
+    readSavedData(): Promise<void> {
+        if (this.currentAttempt === 1) return Promise.resolve();
+
+        const savedData = new Map<string, any>();
+        const promises = new Map<string, Promise<void>>();
+
+        $(this.superActivity.sessionData).find("storage file_directory file_record").each((index, record) => {
+            const attempt = parseInt($(record).find("record_context").attr("attempt") || "-1");
+            const filename = $(record).attr("file_name") || "undefined";
+            if (attempt === this.currentAttempt) {
+                promises.set(filename, new Promise(resolve => {
+                    this.superActivity.loadFileRecord(filename, this.currentAttempt - 1, (result: any) => {
+                        savedData.set(filename, result);
+                        resolve();
+                    });
+                }));
+            }
+        });
+
+        if (promises.has("reset")) return Promise.resolve();
+        else if (promises.has("userdata") && promises.has("feedback")) {
+            return Promise.all(promises.values()).then(() => {
+                this.userDataArray = savedData.get("userdata");
+                this.feedbackArray = savedData.get("feedback");
+            });
+        } else {
+            console.error("Runner.readSavedData: Not the first attempt, but there is no saved data");
+            return Promise.resolve();
+        }
+    }
+
+    private grade(): (FeedbackData | null)[] {
+        return this.activity.parse(this.userDataArray[this.currentIndex]).map((response, i) => {
+            if (!response) return null;
+
+            const match = this.questionArray[this.currentIndex].parts[i].match.get(response);
+            if (!match) {
+                const nomatch = this.questionArray[this.currentIndex].parts[i].nomatch;
+                if (!nomatch) throw new Error(`grade: question ${i} has no match for ${response}`);
+
+                return {
+                    correct: nomatch.score === this.questionArray[this.currentIndex].parts[i].score,
+                    score: nomatch.score,
+                    message: nomatch.message
+                };
+            } else {
+                return {
+                    correct: match.score === this.questionArray[this.currentIndex].parts[i].score,
+                    score: match.score,
+                    message: match.message
+                };
+            }
+        });
+    }
+
+    submit(): void {
+        this.userDataArray[this.currentIndex] = this.activity.read();
+        this.feedbackArray[this.currentIndex] = this.grade();
+
+        this.superActivity.writeFileRecord(
+            "userdata",
+            "application/json",
+            this.currentAttempt,
+            JSON.stringify(this.userDataArray),
+            () => {
+                this.superActivity.writeFileRecord(
+                    "feedback",
+                    "application/json",
+                    this.currentAttempt,
+                    JSON.stringify(this.feedbackArray),
+                    () => {
+                        this.superActivity.scoreAttempt(
+                            "percent",
+                            75, // XX TODO FIX
+                            () => {
+                                this.superActivity.endAttempt(() => {
+                                    this.superActivity.startAttempt((response: Element) => {
+                                        const newAttempt = parseInt(
+                                            $(response)
+                                                .find("attempt_history")
+                                                .attr("current_attempt") || "-1"
+                                        );
+                                        this.currentAttempt = newAttempt;
+                                        this.render();
+                                    });
+                                });
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    }
+
+    reset(): void {
+        this.userDataArray[this.currentIndex] = this.activity.init();
+        this.feedbackArray[this.currentIndex] = this.questionArray[this.currentIndex].parts.map(() => null);
+
+        this.superActivity.writeFileRecord(
+            "reset",
+            "application/json",
+            this.currentAttempt,
+            JSON.stringify(this.userDataArray),
+            () => {
+                this.superActivity.endAttempt(() => {
+                    this.superActivity.startAttempt((response: Element) => {
+                        const newAttempt = parseInt(
+                            $(response)
+                                .find("attempt_history")
+                                .attr("current_attempt") || "-1"
+                        );
+                        this.currentAttempt = newAttempt;
+                        this.render();
+                    });
+                });
+            }
+        );
+    }
+}
