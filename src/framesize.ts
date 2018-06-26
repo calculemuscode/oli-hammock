213a3c88cb1a0f7f1760385f2e5278d92759e189
Rob Simmons <rob@calculem.us>
2018-06-26 12:48:19 -0400

    Prettier

--- src/framesize.ts
+++ src/framesize.ts
@@ -1,39 +1,39 @@
 declare var ResizeObserver: undefined | any;
-    
+
 export class FrameSizer {
     private parentIFrame: null | HTMLIFrameElement;
     private knownHeight: number;
     private needsChecks: boolean;
-    
+
     /**
      * Our document needs to be linked to the parent iframe by a link
-     * 
+     *
      * @param attribute The parent iframe should have this attribute defined
      * @param value The parent iframe's attribute should have this value
      */
     public constructor() {
         const parentIFrame = window.frameElement;
         if (parentIFrame === null) {
-            // Do nothing if we're not inside an iframe            
+            // Do nothing if we're not inside an iframe
             this.parentIFrame = null;
-            this.knownHeight = -1;            
+            this.knownHeight = -1;
             this.needsChecks = false;
         } else if (parentIFrame.tagName !== "IFRAME") {
-            // Give up if we're inside something that is not an iframe            
+            // Give up if we're inside something that is not an iframe
             console.error(`FrameSizer: not inside an <iframe>, cannot resize.`);
             this.parentIFrame = null;
-            this.knownHeight = -1;               
+            this.knownHeight = -1;
             this.needsChecks = false;
         } else {
             this.parentIFrame = parentIFrame as HTMLIFrameElement;
             this.knownHeight = document.body.offsetHeight;
             this.parentIFrame.height = `${this.knownHeight}px`;
 
-            if (typeof ResizeObserver !== 'undefined') {
+            if (typeof ResizeObserver !== "undefined") {
                 // If there's a ResizeObserver (Chrome only as of 2018), then the manual checks aren't needed
                 this.needsChecks = false;
                 new ResizeObserver(() => this.doCheck()).observe(document.body);
-            } else if (typeof MutationObserver !== 'undefined') {
+            } else if (typeof MutationObserver !== "undefined") {
                 // MutationObserver should be present in all modern browsers (IE11, Edge, Safari 6, Chrome, Firefox)
                 this.needsChecks = true;
                 new MutationObserver(() => this.doCheck()).observe(document.body, {
@@ -42,7 +42,7 @@ export class FrameSizer {
                     subtree: true
                 });
             } else {
-                console.error(`FrameSizer: no MutationObserver, this browser does not support resizing.`)
+                console.error(`FrameSizer: no MutationObserver, this browser does not support resizing.`);
                 this.needsChecks = false;
             }
         }
