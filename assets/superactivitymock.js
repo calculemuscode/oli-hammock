21da71caf9f6ac3b341ab32d271f5894f487bbe2
Rob Simmons <rob@calculem.us>
2018-10-03 13:59:48 -0400

    bump version, fix bug in code

--- assets/superactivitymock.js
+++ assets/superactivitymock.js
@@ -9,3 +9,12 @@ function SuperActivityMock() {
     this.loadFileRecord = (x,y,callback) => { throw new Error("Don't know how to do this"); };
     this.logAction = (action, callback) => { console.info(`logged action: ${JSON.stringify(action)}`); setTimeout(() => callback()); };
 }
+
+function ActionLog() { };
+ActionLog.prototype.addSupplement = function(x) { this.supplement = x; }
+
+function SupplementLog() { };
+SupplementLog.prototype.setAction = function(x) { this.action = x; }
+SupplementLog.prototype.setSource = function(x) { this.source = x; }
+SupplementLog.prototype.setInfoType = function(x) { this.infoType = x; }
+SupplementLog.prototype.setInfo = function(x) { this.info = x; }
